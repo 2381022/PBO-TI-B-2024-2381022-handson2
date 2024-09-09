af2ca5b6bd36f9dd2509cbe10d2efa11385bf915
Paul Andrew <2381022@unai.edu>
2024-09-10 00:14:07 +0700

    update handson 2.1

--- branching/SwitchStatement.java
+++ branching/SwitchStatement.java
@@ -8,9 +8,10 @@ public class SwitchStatement {
 
         System.out.println("Masukkan nomor hari (1 - 7) : ");
         int nomorHari = input.nextInt();
+
         String namaHari = "";
 
-        switch (namaHari){
+        switch (nomorHari){
             case 1:
                 namaHari = "Minggu";
                 break;
@@ -36,6 +37,8 @@ public class SwitchStatement {
                 System.out.println("Masukkan nomor hari diantara 1 - 7");
                 System.exit(0);
         }
+        System.out.println("Hari : " + namaHari);
+        input.close();
 
     }
 }
--- expressionvsstatement/ExpressionVsStatement.java
+++ expressionvsstatement/ExpressionVsStatement.java
@@ -1,7 +1,12 @@
 package expressionvsstatement;
 
 public class ExpressionVsStatement {
+    // Expression Vs Statement Vs Block
     public static void main(String[] args) {
-
+        int x = 5;
+        if (x > 5){ // x > 5 -> ekspresi
+            System.out.println(" x lebih besar dari 5");
+        }
+        int a = x + 10;
     }
 }
